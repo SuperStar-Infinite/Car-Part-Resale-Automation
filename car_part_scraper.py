ccc0a10e8ba77bf6786c4e0ac9ef872a4e0c1055
SuperStar-Infinite <superstar.infinite@gmail.com>
2025-07-17 11:14:03 -0400

    Update Chrome Options

--- car_part_scraper.py
+++ car_part_scraper.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # Setup Chrome driver
 options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 # Setup Chrome driver
 driver = webdriver.Chrome(options=options)  # Ensure chromedriver is in PATH
 

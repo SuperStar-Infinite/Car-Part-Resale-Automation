f5d9f4f27e42bd5fbbbf9f526149c2c28ab4ea34
SuperStar-Infinite <superstar.infinite@gmail.com>
2025-07-17 11:38:56 -0400

    Update render-build.sh Context

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -e
+
 STORAGE_DIR=/opt/render/project/.render
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "Installing Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
+
+if [[ ! -d "$STORAGE_DIR/chrome" ]]; then
+  echo "...Downloading Chrome"
+  mkdir -p "$STORAGE_DIR/chrome"
+  cd "$STORAGE_DIR/chrome"
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x google-chrome-stable_current_amd64.deb .
+  dpkg -x google-chrome-stable_current_amd64.deb "$STORAGE_DIR/chrome"
+  cd - >/dev/null
+else
+  echo "...Chrome already installed"
 fi
+
+echo "...Installing Python deps"
+pip install --upgrade pip
 pip install -r requirements.txt
